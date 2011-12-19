2595f296c193d04c290fd830d4e7d58e47163875
wibisono <a.wibisono@uva.nl>
2011-12-19 12:39:36 +0100

    Initial commit just a skeleton, and current choice of libraries (see pom.xml)

---
+++ src/main/java/org/data2semantics/recognize/NERTest.java
@@ -0,0 +1,43 @@
+package org.data2semantics.recognize;
+
+import edu.stanford.nlp.ie.crf.*;
+import edu.stanford.nlp.ie.AbstractSequenceClassifier;
+import edu.stanford.nlp.io.IOUtils;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.CoreAnnotations.AnswerAnnotation;
+
+import java.util.List;
+import java.io.File;
+import java.io.IOException;
+
+import org.data2semantics.util.PDFExtractor;
+
+public class NERTest {
+	/**
+	 * Just a test, recognizing using existing training test.
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+
+		String serializedClassifier = "src\\main\\resources\\all.3class.distsim.crf.ser.gz";
+		File  fileTest				= new File("src\\test\\resources\\neutropenia.pdf");
+		PDFExtractor extractor = new PDFExtractor();
+		
+		AbstractSequenceClassifier classifier = CRFClassifier
+				.getClassifierNoExceptions(serializedClassifier);
+
+		String fileContents = extractor.getText(fileTest);
+		
+		List<List<CoreLabel>> out = classifier.classify(fileContents);
+		for (List<CoreLabel> sentence : out) {
+			for (CoreLabel word : sentence) {
+				System.out.print(word.word() + '/'
+						+ word.get(AnswerAnnotation.class) + ' ');
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
