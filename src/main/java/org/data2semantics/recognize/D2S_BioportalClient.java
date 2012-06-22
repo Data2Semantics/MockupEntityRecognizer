109eb03e2c7604f54b77989e0aeb1f34caf53920
Rinke Hoekstra <rjhoekstr@gmail.com>
2012-06-22 14:06:58 +0200

    Retrying 10 times if we get a non 200 response from the BioPortal annotator

--- src/main/java/org/data2semantics/recognize/D2S_BioportalClient.java
+++ src/main/java/org/data2semantics/recognize/D2S_BioportalClient.java
@@ -3,6 +3,7 @@ package org.data2semantics.recognize;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -13,15 +14,18 @@ import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.util.EntityUtils;
 
 public class D2S_BioportalClient {
 	public static final String annotatorUrl = "http://rest.bioontology.org/obs/annotator";
-	DefaultHttpClient client = new DefaultHttpClient();
+	DefaultHttpClient client  = new DefaultHttpClient();
 	HttpPost method = new HttpPost(annotatorUrl);
 	List <NameValuePair> params = new ArrayList <NameValuePair>();
 
@@ -35,7 +39,7 @@ public class D2S_BioportalClient {
 
 		
 		// Configure the form parameters
-		params.add(new BasicNameValuePair("longestOnly", "true"));
+		params.add(new BasicNameValuePair("longestOnly", "false"));
 		params.add(new BasicNameValuePair("wholeWordOnly", "true"));
 		params.add(new BasicNameValuePair("filterNumber", "true"));
 		params.add(new BasicNameValuePair("stopWords", ""));
@@ -54,6 +58,35 @@ public class D2S_BioportalClient {
 		
 	}
 
+	
+
+	private HttpEntity callBioportal() throws IOException,
+			ClientProtocolException, InterruptedException, Exception {
+		StatusLine status;
+		HttpResponse response;
+		HttpEntity entity;
+		int tries = 0;
+		
+		do {
+			System.out.println("Contacting BioPortal..");
+			response = client.execute(method);
+			entity = response.getEntity();
+			status = response.getStatusLine();
+			System.out.println(status);
+			if (entity != null && status.getStatusCode() != 200) {
+				EntityUtils.consume(entity);
+				System.out.println("Retrying in 2 secs...");
+				Thread.sleep(2000);
+			}
+			tries += 1;
+		} while (status.getStatusCode() != 200 && tries < 10 ); 
+		
+		if (tries == 10) throw new Exception("Could not retrieve annotation from BioPortal Annotator");
+		
+		
+		return entity;
+	}
+	
 	/**
 	 * 
 	 * This is where you annotate a text and get the result. 
@@ -75,10 +108,8 @@ public class D2S_BioportalClient {
 		
 		try {
 
-			HttpResponse response = client.execute(method);
-			System.out.println(response.getStatusLine());
-			
-			HttpEntity entity = response.getEntity();
+			HttpEntity entity = callBioportal();
+			System.out.println("Retrieving results from BioPortal Annotator");
 			
 			if (entity != null) {
 				InputStream contentStream = entity.getContent();
@@ -98,6 +129,7 @@ public class D2S_BioportalClient {
 		}
 		return result.toString();
 	}
+
 	
 	public void annotateToFile(String text, String format, File outputFile){
 		params.add(new BasicNameValuePair("textToAnnotate",text));
@@ -109,13 +141,15 @@ public class D2S_BioportalClient {
 		
 		try {
 
-			HttpResponse response = client.execute(method);
-			System.out.println(response.getStatusLine());
+			HttpEntity entity = callBioportal();
+			System.out.println("Retrieving results from BioPortal Annotator");
 			
 			OutputStream outputStream =null;
 			InputStream contentStream =null;
 			
-			HttpEntity entity = response.getEntity();
+			
+			
+			System.out.println("Retrieving "+ entity.getContentLength() + " bytes");
 			
 			if (entity != null) {
 				contentStream = entity.getContent();
