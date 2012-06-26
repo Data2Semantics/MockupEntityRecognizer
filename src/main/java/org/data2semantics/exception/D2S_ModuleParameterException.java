7d3c9c82cbf9cdd5f82ee5014f5e183dd6ab1e94
wibisono <a.wibisono@uva.nl>
2012-06-26 14:09:40 +0200

    Add some exception classes related to modules, to add additional info on top of current exceptions

---
+++ src/main/java/org/data2semantics/exception/D2S_ModuleParameterException.java
@@ -0,0 +1,26 @@
+package org.data2semantics.exception;
+
+
+/**
+ * Adding information with regards to required parameter for modules.
+ * @author wibisono
+ *
+ */
+public class D2S_ModuleParameterException extends D2S_ModuleException{
+	public D2S_ModuleParameterException() {
+		super("One of this D2S Modules parameter is not valid");
+	}
+	public D2S_ModuleParameterException(String message){
+		super(message);
+	}
+	
+	public D2S_ModuleParameterException(Throwable t){
+		super(t);
+	}
+	
+	public D2S_ModuleParameterException(String message, Throwable t){
+		super(message,t);
+	}
+	
+
+}
